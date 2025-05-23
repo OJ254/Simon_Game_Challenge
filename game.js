4e39dc42c7ab43304bcbd6cd0e6ab67a01cf9e15
Japheth Oruko <japhethoruko@gmail.com>
2025-05-23 15:51:18 +0300

    improve 'progress Simon_Game_Challenge

--- game.js
+++ game.js
@@ -1,10 +1,10 @@
-// var gamePattern = [];
-// var buttonColours = ["red", "blue", "green", "yellow"];
-// var randomChosenColour = gamePattern.push(buttonColours[nextSequence()]);
-//
-// function nextSequence() {
-//     return Math.floor(Math.random() * 4);
-// }
-//
-// console.log(randomChosenColour);
-// console.log(gamePattern);
+var gamePattern = [];
+var buttonColours = ["red", "blue", "green", "yellow"];
+var randomChosenColour = gamePattern.push(buttonColours[nextSequence()]);
+
+function nextSequence() {
+    return Math.floor(Math.random() * 4);
+}
+
+console.log(randomChosenColour);
+console.log(gamePattern);
